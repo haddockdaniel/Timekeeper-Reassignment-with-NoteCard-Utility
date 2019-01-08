3f8338ab4870a9c3ce8f77728fbfc8835d8e1f45
haddockdaniel <43544084+haddockdaniel@users.noreply.github.com>
2019-01-08 13:06:46 -0500

    update

--- JurisUtilityBase/ReportDisplay.Designer.cs
+++ JurisUtilityBase/ReportDisplay.Designer.cs
@@ -37,9 +37,14 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ShowEditingIcon = false;
             this.dataGridView1.Size = new System.Drawing.Size(1000, 387);
             this.dataGridView1.TabIndex = 0;
             // 
